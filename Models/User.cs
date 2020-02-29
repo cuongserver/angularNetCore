8c48c3a9cb1139a892b5ab873273f6108e51d158
cuongserver <ndcuong.cf@gmail.com>
2020-02-29 12:45:57 +0700

    change

--- Models/User.cs
+++ Models/User.cs
@@ -9,7 +9,7 @@ namespace AngularNETcore.Models
     {
         public string userName { get; set; }
         public string userPass { get; set; }
-        public string userFullName{get; set;}
+        public string userFullNamexx {get; set;}
     }
 
     public class LoginValidationStatus
