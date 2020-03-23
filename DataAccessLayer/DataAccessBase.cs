43a3775997ca6e63703bc76a9196a0d65ca4ae34
cuongserver <59495181+cuongserver@users.noreply.github.com>
2020-03-23 19:04:09 +0700

    upload 2020

--- DataAccessLayer/DataAccessBase.cs
+++ DataAccessLayer/DataAccessBase.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -53,36 +53,7 @@ namespace AngularNETcore.DataAccessLayer
 
             return dt;
         }
-
-        protected void DumpTableToFile(SqlCommand cmd, string destinationFile)
-        {
-            using (var reader = cmd.ExecuteReader())
-            using (var outFile = File.CreateText(destinationFile))
-            {
-                string[] columnNames = GetColumnNames(reader).ToArray();
-                int numFields = columnNames.Length;
-                outFile.WriteLine(string.Join(",", columnNames));
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-                        string[] columnValues =
-                            Enumerable.Range(0, numFields)
-                                      .Select(i => reader.GetValue(i).ToString())
-                                      .Select(field => string.Concat("\"", field.Replace("\"", "\"\""), "\""))
-                                      .ToArray();
-                        outFile.WriteLine(string.Join(",", columnValues));
-                    }
-                }
-            }
-        }
-        private IEnumerable<string> GetColumnNames(IDataReader reader)
-        {
-            foreach (DataRow row in reader.GetSchemaTable().Rows)
-            {
-                yield return (string)row["ColumnName"];
-            }
-        }
+        
 
     }
 }
