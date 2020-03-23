00295213c323f25951822342281b2ec307612119
cuongserver <59495181+cuongserver@users.noreply.github.com>
2020-03-23 18:57:56 +0700

    upload 2020

---
+++ Models/SystemSetting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularNETcore.Models
+{
+    public class Holiday
+    {
+        public string holidayDate { get; set; }
+        public string description { get; set; }
+        public bool isEnabled { get; set; }
+    }
+    public class Holidays
+    {
+        public List<Holiday> holidays { get; set; }
+        public string status { get; set; }
+        public string message { get; set; }
+    }
+}
